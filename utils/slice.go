ca6dde0b5a9c34b29d63c82d61c8c0c54cfe6652
fanjindong <765912710@qq.com>
2020-09-09 12:05:27 +0800

    feat: add SliceShuffle, SliceContain, SliceContainsAny, SliceContainsAll

--- utils/slice.go
+++ utils/slice.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"math/rand"
 	"reflect"
 )
 
@@ -27,4 +28,110 @@ func SliceInsert(s interface{}, index int, item interface{}) error {
 	newSlice.Index(index).Set(itemValue)
 	v.Set(newSlice)
 	return nil
+}
+
+//func SliceElemsFormat(elems ...interface{}) []string {
+//	ss := make([]string, 0, len(elems))
+//	for i, elem := range elems {
+//		switch e := elem.(type) {
+//		case string:
+//			ss[i] = e
+//		case int:
+//			ss[i] = strconv.FormatInt(int64(e), 10)
+//		case int8:
+//			ss[i] = strconv.FormatInt(int64(e), 10)
+//		case int16:
+//			ss[i] = strconv.FormatInt(int64(e), 10)
+//		case int32:
+//			ss[i] = strconv.FormatInt(int64(e), 10)
+//		}
+//	}
+//	return ss
+//}
+
+//SliceShuffle 用于打乱一个切片
+func SliceShuffle(slice interface{}) {
+	v := reflect.ValueOf(slice)
+	swap := reflect.Swapper(slice)
+	for n := v.Len() - 1; n > 0; n-- {
+		rIndex := rand.Intn(n)
+		swap(rIndex, n)
+	}
+}
+
+//SliceContain 判断一个数组(切片)是否包含某一元素(支持[]int, []string, []Struct, []*Struct等)
+func SliceContain(slice interface{}, value interface{}) bool {
+	return SliceContainsAny(slice, value)
+}
+
+//SliceContainsAny 判断一个数组(切片)是否包含任一元素(支持[]int, []string, []Struct, []*Struct等)
+// value in slice: return true
+// value not in slice: return false
+// slice not in (array, slice): return false
+func SliceContainsAny(slice interface{}, values ...interface{}) bool {
+	rt := reflect.TypeOf(slice)
+	rv := reflect.ValueOf(slice)
+	rtk := rt.Kind()
+	if rtk == reflect.Ptr {
+		rtk = rt.Elem().Kind()
+		rv = rv.Elem()
+	}
+	switch rt.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < rv.Len(); i++ {
+			rvi := rv.Index(i)
+			switch rvi.Kind() {
+			case reflect.Uintptr, reflect.Array, reflect.Slice, reflect.Interface, reflect.Ptr, reflect.Struct, reflect.Map, reflect.Func, reflect.UnsafePointer:
+				for _, value := range values {
+					if reflect.DeepEqual(rvi.Interface(), value) {
+						return true
+					}
+				}
+			default:
+				for _, value := range values {
+					if rvi.Interface() == value {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
+}
+
+//SliceContainsAll 判断一个数组(切片)是否包含所有元素(支持[]int, []string, []Struct, []*Struct等)
+func SliceContainsAll(slice interface{}, values ...interface{}) bool {
+	rt := reflect.TypeOf(slice)
+	rv := reflect.ValueOf(slice)
+	rtk := rt.Kind()
+	if rtk == reflect.Ptr {
+		rtk = rt.Elem().Kind()
+		rv = rv.Elem()
+	}
+	switch rt.Kind() {
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < rv.Len(); i++ {
+			rvi := rv.Index(i)
+			switch rvi.Kind() {
+			case reflect.Uintptr, reflect.Array, reflect.Slice, reflect.Interface, reflect.Ptr, reflect.Struct, reflect.Map, reflect.Func, reflect.UnsafePointer:
+				for i, value := range values {
+					if reflect.DeepEqual(rvi.Interface(), value) {
+						values = append(values[:i], values[i+1:]...)
+						break
+					}
+				}
+			default:
+				for i, value := range values {
+					if rvi.Interface() == value {
+						values = append(values[:i], values[i+1:]...)
+						break
+					}
+				}
+			}
+			if len(values) == 0 {
+				return true
+			}
+		}
+	}
+	return false
 }
--- utils/slice_test.go
+++ utils/slice_test.go
@@ -1,6 +1,8 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 func TestSliceInsert(t *testing.T) {
@@ -42,5 +44,106 @@ func TestSliceInsert(t *testing.T) {
 				assert.Equal(t, (*got)[i], want[i])
 			}
 		}
+	}
+}
+
+// BenchmarkSliceInsert-4   	  2000000	    594 ns/op
+func BenchmarkSliceInsert(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "g"}
+		index := i % len(s)
+		_ = SliceInsert(&s, index, "0")
+	}
+}
+
+func TestSliceShuffle(t *testing.T) {
+	type args struct {
+		slice interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "1", args: args{slice: []int{1, 2, 3, 4, 5}}},
+		{name: "2", args: args{slice: []string{"a", "b", "c", "d", "e"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Log(tt.args.slice)
+			SliceShuffle(tt.args.slice)
+			t.Log(tt.args.slice)
+		})
+	}
+}
+
+func TestSliceContain(t *testing.T) {
+	type args struct {
+		slice interface{}
+		value interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "1", args: args{slice: []int{1, 2, 3, 4, 5}, value: 3}, want: true},
+		{name: "2", args: args{slice: []int{1, 2, 3, 4, 5}, value: 5}, want: true},
+		{name: "3", args: args{slice: []int{1, 2, 3, 4, 5}, value: 6}, want: false},
+		{name: "4", args: args{slice: []int{1, 2, 3, 4, 5}, value: 0}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceContain(tt.args.slice, tt.args.value); got != tt.want {
+				t.Errorf("SliceContain() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceContainsAny(t *testing.T) {
+	type args struct {
+		slice  interface{}
+		values []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "1", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{1, 2}}, want: true},
+		{name: "2", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{4, 2}}, want: true},
+		{name: "3", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{0, 3}}, want: true},
+		{name: "4", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{0, 6}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceContainsAny(tt.args.slice, tt.args.values...); got != tt.want {
+				t.Errorf("SliceContainsAny() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceContainsAll(t *testing.T) {
+	type args struct {
+		slice  interface{}
+		values []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "1", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{1, 2, 3}}, want: true},
+		{name: "2", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{1, 3}}, want: true},
+		{name: "3", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{1}}, want: true},
+		{name: "4", args: args{slice: []int{1, 2, 3, 4, 5}, values: []interface{}{0, 2, 3}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceContainsAll(tt.args.slice, tt.args.values...); got != tt.want {
+				t.Errorf("SliceContainsAll() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
